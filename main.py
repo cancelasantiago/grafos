9a6501185a009f41fc100c5efbbd3b996ee176ea
cancelasantiago <cancelasantiago@gmail.com>
2022-01-02 17:21:51 -0300

    Varias implementaciones

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-from mtxp import insertar_nodos_ady
-n = []
-insertar_nodos_ady(n)
+import mtxp
+g = []
+
+print('Selecciona una opción: ')
+print('     1)Ingresar nodos.')
+print('     2)Ingresar aristas bidireccionales.')
+print('     3)Ingresar arista unidireccional.')
+print('     4)Imprimir grafo.')
+print('     5)Salir.')
+o = input('Opción: ')
+while o != '5':
+    if o == '1':
+        mtxp.insertar_nodos(g)
+    elif o == '2':
+        a = input('Vertice 1: ')
+        b = input('Vertice 2: ')
+        mtxp.agregar_arista_bidir(g, a, b)
+    elif o == '3':
+        a = input('Vertice 1: ')
+        b = input('Vertice 2: ')
+        mtxp.agregar_arista_unidir(g, a, b)
+    elif o == '4':
+        print(g)
+    print('Selecciona una opción: ')
+    print('     1)Ingresar nodos.')
+    print('     2)Ingresar aristas bidireccionales.')
+    print('     3)Ingresar arista unidireccional.')
+    print('     4)Imprimir grafo.')
+    print('     5)Salir.')
+    o = input('Opción: ')
--- mtxp.py
+++ mtxp.py
@@ -1,52 +1,97 @@
 import array as ar
 
-def insertar_nodos_ady(n):
+def insertar_nodos_ady(g):
     a = input('Ingresar nodo adyacente 1: ')
     b = input('Ingresar nodo adyacente 2: ')
 
     while a != 'x' or b !='x':
-        if not n:
-            n.append([a,b])
-            n.append([b,a])
+        if not g:
+            g.append([a,b])
+            g.append([b,a])
         else:
-            estaA = False #indica si a esta en n[i][0]
-            estaB = False #indica si a esta en n[i][0]
+            estaA = False #indica si a esta en g[i][0]
+            estaB = False #indica si a esta en g[i][0]
             i=0
-            while (not estaA or not estaB) and i<len(n):
-                if a == n[i][0]:
+            while (not estaA or not estaB) and i<len(g):
+                if a == g[i][0]:
                     estaA = True
-                    if b not in n[i]:
-                        n[i].append(b)
+                    if b not in g[i]:
+                        g[i].append(b)
                     i = i+1
-                    while not estaB and i<len(n):
-                        if b == n[i][0]: 
+                    while not estaB and i<len(g):
+                        if b == g[i][0]: 
                             estaB = True
-                            if a not in n[i]:
-                                n[i].append(a)
+                            if a not in g[i]:
+                                g[i].append(a)
                         i = i+1
                 else:
-                    if b == n[i][0]:
+                    if b == g[i][0]:
                         estaB = True
-                        if a not in n[i]:
-                            n[i].append(a)
+                        if a not in g[i]:
+                            g[i].append(a)
                         i = i+1
-                        while not estaA and i<len(n):
-                            if a == n[i][0]: 
+                        while not estaA and i<len(g):
+                            if a == g[i][0]: 
                                 estaA = True
-                                if b not in n[i]:
-                                    n[i].append(b)
+                                if b not in g[i]:
+                                    g[i].append(b)
                             i = i+1
                     else: i=i+1
-            if not estaB and estaA and i>=len(n):
-                n.append([b,a])
+            if not estaB and estaA and i>=len(g):
+                g.append([b,a])
             else:
-                if not estaA and estaB and i>=len(n):
-                    n.append([a,b])
+                if not estaA and estaB and i>=len(g):
+                    g.append([a,b])
                 else: 
-                    if not estaA and not estaB and i>=len(n):
-                        n.append([a,b])
-                        n.append([b,a])
+                    if not estaA and not estaB and i>=len(g):
+                        g.append([a,b])
+                        g.append([b,a])
             i = 0
-        print(n)
+        print(g)
         a = input('Ingresar nodo adyacente 1: ')
         b = input('Ingresar nodo adyacente 2: ')
+
+def insertar_nodos(g):
+    print('Si no quiere ingresar más nodos, el nombre del mismo debe ser "x".')
+    nodo = input('Ingresar el nombre del nodo: ')
+    while nodo != 'x':
+        if nodo not in g:
+            g.append([nodo])
+        else:
+            print('Ya ingresó ese nodo.')
+        nodo = input('Ingresar el nombre del nodo: ')
+    #print(g)
+
+def agregar_arista_bidir(g, a, b):
+    l = len(g)
+    estaA = False
+    estaB = False
+    for i in range(l):
+        if a == g[i][0]:
+            estaA = True
+            aix = i
+        if b == g[i][0]:
+            estaB = True
+            bix = i
+    if estaA and estaB:
+        g[aix].append(b)
+        g[bix].append(a)
+    else:
+        print('Uno de los nodos no se encuentra.')
+
+def agregar_arista_unidir(g, a, b):
+    l = len(g)
+    estaA = False
+    estaB = False
+    for i in range(l):
+        if a == g[i][0]:
+            estaA = True
+            aix = i
+        if b == g[i][0]:
+            estaB = True
+            bix = i
+    if estaA and estaB:
+        g[aix].append(b)
+    else:
+        print('Uno de los nodos no se encuentra.')
+
